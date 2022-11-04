4d25b3a6a62e96fc399b1757509b46604208e9e1
Gabriel Medeiros <gabrielmedeirosvino@gmail.com>
2022-11-04 16:59:16 -0300

    posts e get feitos, faltando apenas renderizar os ultimos 10 tweets

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const arrTweets = []
+const arrUser = []
+
+app.post("/sign-up", (req, res) =>{
+    const newUser = {
+        username: req.body.username,
+		avatar: req.body.avatar
+    }
+    arrUser.push(newUser)
+    res.send("OK")
+})
+
+app.post("/tweets", (req, res)=>{
+const img = arrUser.find((obj)=>obj.username === req.body.username)
+
+    const newTweet = {
+        username: req.body.username,
+        avatar: img.avatar,
+        tweet: req.body.tweet
+    }
+
+    arrTweets.unshift(newTweet)
+    res.send("OK")
+})
+
+app.get("/tweets", (req,res)=>{
+        res.send(arrTweets)
+    
+})
+app.listen(5000, ()=> console.log("App running in port: 5000"))
